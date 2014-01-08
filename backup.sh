e6ef36ba29236e5e105bf01edafff77b51b11bdd
petrux <giulio.petrucci@gmail.com>
2014-01-08 17:02:26 +0100

    backup and restore of packages, repository keys, sources list and python packages (via pip)

--- backup.sh
+++ backup.sh
@@ -5,16 +5,39 @@
 # empty (i.e. -z), then use the default one.
 # Leave empty if you want "/" to be the backup
 # destination directory.
-DEF_DEST_DIR= 
+DEF_DEST_DIR=/tmp
 DEST_DIR=$1
 if [ -z $1 ]; then
 	DEST_DIR=$DEF_DEST_DIR
 fi
 
-# generate the backup archive file name
-DEST_FILE=$DEST_DIR/backup-`date +%Y-%m-%d_%H-%M-%S`.tgz
-echo "backup file:" $DEST_FILE
+# create the actual backup subdir
+DEST_DIR=$DEST_DIR/`whoami`-`hostname`-backup-`date +%Y-%m-%d_%H-%M-%S`
+echo "user:" `whoami`
+echo "host:" `hostname`
+echo "backup directory:" $DEST_DIR
+
+echo "creating the backup directory..."
+mkdir $DEST_DIR
+
+echo "repository keys..."
+sudo apt-key exportall > $DEST_DIR/Repo.keys
+
+echo "sources list..."
+sudo cp /etc/apt/sources.list $DEST_DIR/sources.list
+
+echo "packages list..."
+dpkg --get-selections > $DEST_DIR/Package.list
+
+echo "python packages (via pip)..."
+pip freeze > $DEST_DIR/requirements.txt
+
+cp restore.sh $DEST_DIR
+chmod 775 $DEST_DIR/restore.sh
 
 # GO!
 # exlude: DEST_DIR, /media, /proc, /lost+found, /mnt, /sys
 #sudo tar -cvpzf $DEST_FILE --exclude=$DEST_DIR --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt --exclude=/sys /
+
+echo "done!"
+echo
--- restore.sh
+++ restore.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-# sudo tar -xvpzf /backup.tgz -C /
-# sudo mkdir /proc /lost+found /mnt /sys
-echo "...coming soon! :-)"
+
+# this restore file is intended to be placed
+# within the backup directory created by the
+# ./backup.sh script.
+
+echo "restoring repository keys..."
+sudo apt-key add Repo.keys
+
+echo "restoring sources list (and update)..."
+sudo cp sources.list /etc/apt/sources.list 
+sudo apt-get update
+
+echo "restoring packages..."
+sudo apt-get install dselect
+sudo dpkg --set-selections < Package.list
+sudo apt-get dselect-upgrade -y
+
+echo "restoring python packages (via pip)..."
+pip install -r requirements.txt
