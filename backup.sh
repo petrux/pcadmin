bac68001c53e46e151992b321fa121e33cd25ff3
petrux <giulio.petrucci@gmail.com>
2014-01-09 17:55:28 +0100

    improved

--- backup.sh
+++ backup.sh
@@ -13,31 +13,17 @@ fi
 
 # create the actual backup subdir
 DEST_DIR=$DEST_DIR/`whoami`-`hostname`-backup-`date +%Y-%m-%d_%H-%M-%S`
-echo "user:" `whoami`
-echo "host:" `hostname`
-echo "backup directory:" $DEST_DIR
 
-echo "creating the backup directory..."
 mkdir $DEST_DIR
-
-echo "repository keys..."
 sudo apt-key exportall > $DEST_DIR/Repo.keys
-
-echo "sources list..."
 sudo cp /etc/apt/sources.list $DEST_DIR/sources.list
-
-echo "packages list..."
 dpkg --get-selections > $DEST_DIR/Package.list
-
-echo "python packages (via pip)..."
 pip freeze > $DEST_DIR/requirements.txt
-
+rsync -a --progress --exclude=Downloads --exclude=Dropbox --exclude= /home/`whoami` $DEST_DIR
+rsync -a --progress /opt $DEST_DIR
 cp restore.sh $DEST_DIR
 chmod 775 $DEST_DIR/restore.sh
 
-# GO!
-# exlude: DEST_DIR, /media, /proc, /lost+found, /mnt, /sys
-#sudo tar -cvpzf $DEST_FILE --exclude=$DEST_DIR --exclude=/media --exclude=/proc --exclude=/lost+found --exclude=/mnt --exclude=/sys /
-
-echo "done!"
+echo $DEST_DIR " ...done!"
+echo 
 echo
--- restore.sh
+++ restore.sh
@@ -4,17 +4,12 @@
 # within the backup directory created by the
 # ./backup.sh script.
 
-echo "restoring repository keys..."
+sudo rsync -a --progress `whoami` /home/
+sudo rsync -a --progress ./opt /
 sudo apt-key add Repo.keys
-
-echo "restoring sources list (and update)..."
 sudo cp sources.list /etc/apt/sources.list 
 sudo apt-get update
-
-echo "restoring packages..."
 sudo apt-get install dselect
 sudo dpkg --set-selections < Package.list
 sudo apt-get dselect-upgrade -y
-
-echo "restoring python packages (via pip)..."
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
